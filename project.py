d7b338ba6286a289e35a5597d14d486b71707e53
Ethan Keirs <60351308+EthanKeirs@users.noreply.github.com>
2021-06-08 23:14:30 +1000

    Update project.py

--- project.py
+++ project.py
@@ -21,23 +21,20 @@ def distance():
 # set Trigger after 0.01ms to LOW
 	time.sleep(0.00001)
 	GPIO.output(GPIO_TRIGGER, False)
- 
 	StartTime = time.time()
 	StopTime = time.time()
  
 #  StartTime
 	while GPIO.input(GPIO_ECHO) == 0:
 		StartTime = time.time()
- 
 #  StopTime
 	while GPIO.input(GPIO_ECHO) == 1:
 		StopTime = time.time()
  
 # time difference between start and arrival
 	TimeElapsed = StopTime - StartTime
-# multiply with the sonic speed (34300 cm/s) and divide by 2, because there and back
+# multiply with the sonic speed (34300 cm/s) and divide by 2
 	distance = (TimeElapsed * 34300) / 2
- 
     return distance
 
 i = True
@@ -47,8 +44,3 @@ while i == True:
 		print ("Measured Distance = %.1f cm" % dist)
 		requests.post('https://maker.ifttt.com/trigger/WithInRange/with/key/dcn2s_hlgJgtrV0gZ4Bu0A')
 		i = False
-		
-        # Reset by pressing CTRL + C
-except KeyboardInterrupt:
-	print("Measurement stopped by User")
-	GPIO.cleanup()
